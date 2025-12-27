c2164aaf3e772f7a292b0376cdc34972d61bbfbe
FRANKEN25 <faisal25051999@gmail.com>
2025-12-27 17:45:57 +0800

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,15 @@
+document.getElementById('about').addEventListener("click",function(){
+    window.location.href = "about.html"
+})
+
+document.getElementById('portofolio').addEventListener('click',function(){
+    window.location.href="portofolio.html"
+})
+
+document.getElementById('contact').addEventListener('click', function(){
+    window.location.href="contact.html"
+})
+
+function goBack(){
+    window.location.href="index.html"
+}
